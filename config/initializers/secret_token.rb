5a43b0acf1cbb538c5c79628ea8100f04cc163d0
Neha <neha@weboniselab.com>
2013-03-20 14:58:48 +0530

    my first commit after new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniPivotal::Application.config.secret_token = 'fc16de0f1f811f84bfe71da43f9df973f9ce3f6c09a2c51099835752d409dc7da4b9ab6672ecbe4cb671472bcab27f51503617ce4654ab30f3c6e4b0a2af2156'
