1bf0be3bd6a0dc5a3988f1036be25dcba9eb87b1
Neha <neha@weboniselab.com>
2013-03-25 19:41:52 +0530

    added comments and task fields and working on date picker for sprint creation

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,13 +1,22 @@
 module ProjectsHelper
 
-  #Helper Method to find a user's projects
+  #Method:Helper to find a user's projects
   def projects
     projects = current_user.projects
   end
 
+  #Method:Helper to check if the user signed-in is the owner of the project to allow sprint creation
   def project_owner(project, user)
     if project.project_users.where(:user_id => user.id).first.role == 'owner'
       return true
+    end
+  end
+
+  #Method:Helper to check if the currently opened project has any sprints.
+  #Used for displaying Sprints in the show action of Projects
+  def has_sprints
+    if @project.sprints.present?
+      return @project.sprints
     end
   end
 end
